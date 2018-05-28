1011dcba5a8383e35be1e3b45779501a2f86d9a2
jonashw <2jonwilson@gmail.com>
2018-05-28 04:31:54 -0700

    added a particle system to celebrate a completed level

--- src/Level.js
+++ src/Level.js
@@ -3,26 +3,34 @@ function Level(subjects){
     var _activeSubjects = subjects.slice();
     var _nextIndex = nextIndex();
 
+    var _fountain;
+
     this.activate = () => {
     };
 
     this.reset = () => {
         _bg = color(255);
         _activeSubjects = subjects.slice();
+        _fountain.Stop();
+        _fountain = null;
     };
 
     this.draw = () => {
         background(_bg);
         if(!_activeSubjects.length){
             push();
+            _fountain.Draw();
+            _fountain.Create();
+            _fountain.Step();
             rectMode(CENTER);
             textAlign(CENTER);
             noStroke();
-            textSize(72);
+            textSize(100);
             text("Great job!!!", width/2,height/2);
             pop();
             return;
         }
+        fill(0);
         let c = _activeSubjects[_nextIndex].colorName;
         push();
         rectMode(CENTER);
@@ -41,17 +49,42 @@ function Level(subjects){
             return;
         }
         let activated = _activeSubjects.filter(d => d.containsPoint(p))[0];
-        if(activated){
-            let index = _activeSubjects.indexOf(activated);
-            if(index == _nextIndex){
-                _bg = activated.color;
-                _activeSubjects.splice(index,1);
-                _nextIndex = nextIndex();
-            }
+        if(!activated){
+            return;
+        }
+        let index = _activeSubjects.indexOf(activated);
+        if(index == _nextIndex){
+            _bg = activated.color;
+            _activeSubjects.splice(index,1);
+            _nextIndex = nextIndex();
+        }
+        if(_activeSubjects.length == 0){
+            _fountain = initFountain();
         }
     };
 
     function nextIndex(){
         return floor(random(0,_activeSubjects.length));
+   }
+
+   function initFountain(){
+        return new Fountain({
+            "parts": [
+            {
+                "name": "foo",
+                "color": [ "white", [ 120, 100, 50 ], [200,100,50]],
+                "shape": "rect",
+                "gravity": 0.1,
+                "sizePercent": 1,
+                "angle": [ 240, 300 ],
+                "speed": 9.5,
+                //"limit": 250,
+                "lifetime": 250,
+                "size": [ 8, 12 ],
+                "x": 0.5,
+                "y": 0.5
+            }
+            ]
+        }, 'foo');
    }
 }
